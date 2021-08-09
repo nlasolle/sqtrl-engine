5bdad8a0c2641c9d54658c1ddf8ab0b7891c0c07
LASOLLE Nicolas <nicolas.lasolle@univ-lorraine.fr>
2021-08-09 12:13:29 +0200

    Maven project initialization

---
+++ src/test/java/org/ahp/sqtrl_engine/RuleFileManagementTest.java
@@ -0,0 +1,14 @@
+package org.ahp.sqtrl_engine;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RuleFileManagementTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
