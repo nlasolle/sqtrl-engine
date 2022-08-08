0b0dc4458baefc543a11564c42df7a99e7d0e314
LASOLLE Nicolas <nicolas.lasolle@univ-lorraine.fr>
2022-08-08 11:41:20 +0200

    Jena Wrapper refactoring

--- src/main/java/org/ahp/sqtrlengine/dao/JenaWrapper.java
+++ src/main/java/org/ahp/sqtrlengine/dao/JenaWrapper.java
@@ -1,107 +1,122 @@
 package org.ahp.sqtrlengine.dao;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.List;
-import java.util.logging.Logger;
-
 import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.query.ResultSet;
 import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.RDFReaderI;
 import org.apache.jena.rdfconnection.RDFConnection;
 import org.apache.jena.rdfconnection.RDFConnectionFactory;
-import org.apache.jena.update.UpdateAction;
 import org.apache.jena.update.UpdateExecutionFactory;
 import org.apache.jena.update.UpdateFactory;
 import org.apache.jena.update.UpdateProcessor;
 import org.apache.jena.update.UpdateRequest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
- * Interface to dialog with an RDF base using Jena API
+ * Interface to dialog with an RDF base by executing SPARQL queries using Jena API
  * @author Nicolas Lasolle
  *
  */
 public class JenaWrapper {
 
-	private static final String DEFAULT_FORMAT = "TURTLE";
 
-	//RDF Model containing all the triples of the database (multiple files)
-	Model model = ModelFactory.createDefaultModel();
+	private RDFConnection conn;
+	private QueryExecution qExec;
+	private String endpoint;
 
-	Model inferenceModel = ModelFactory.createDefaultModel();
-
-	//This model is used to store the current model
-	Model updateModel = ModelFactory.createDefaultModel(); 
-
-	String updateFile;
-
-	RDFConnection conn;
-	QueryExecution qExec;
-	private static Logger logger = Logger.getLogger(JenaWrapper.class.getName());
+	private static Logger logger = LogManager.getLogger(JenaWrapper.class);
 
 	/**
-	 * 
-	 * @param endpoint sparql endpoint iri
+	 * Creating JenaWrapper for a given endpoint
+	 * @param endpoint SPARQL endpoint iri
 	 */
 	public JenaWrapper(String endpoint) {
+		logger.info("Configuring Jena Wrapper for executing SPARQL queries over the endpoint <{}>", endpoint);
 		conn = RDFConnectionFactory.connect(endpoint);
+		this.endpoint = endpoint;
 	}
 
-	public void loadGraph(String file) {
-
-		//filesList should never be empty
-		if(file.isBlank()) {
-			throw new IllegalArgumentException("No RDF file provided.");
-		}
-
-		//Construct the model by loading all files in the RDF directory (which is a property to be configured in the .yml file)
-		RDFReaderI localReader = model.getReader(DEFAULT_FORMAT);
-		localReader.setProperty("WARN_REDEFINITION_OF_ID","EM_IGNORE");
-
-		try {
-			localReader.read(model, new FileInputStream(file), file);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-
-
-		inferenceModel = ModelFactory.createRDFSModel(model);
-
+	/**
+	 * Should be called after iterating over the result of a query execution
+	 */
+	public void closeExecution() {
+		qExec.close();
 	}
 
-	public void loadGraph(List<String> files) {
+	/**
+	 * Execute a SELECT SPARQL query using a read only or update SPARQL endpoint
+	 * @param queryString the SELECT SPARQL query as a string
+	 * @return the results of the query
+	 */
+	public ResultSet executeSelectQuery(String queryString) {	
+		Query query = QueryFactory.create(queryString) ;
+		return executeSelectQuery(query);
+	}
 
-		//filesList should never be empty
-		if(files.isEmpty()) {
-			throw new IllegalArgumentException("There is no element in the list of RDF files.");
-		}
 
-		//Construct the model by loading all files in the RDF directory (which is a property to be configured in the .yml file)
-		RDFReaderI localReader = model.getReader(DEFAULT_FORMAT);
-		localReader.setProperty("WARN_REDEFINITION_OF_ID","EM_IGNORE");
+	/**
+	 * Execute a SELECT SPARQL query using a read  only or update SPARQL endpoint
+	 * @param queryString the SELECT SPARQL query
+	 * @return the results of the query
+	 */
+	public ResultSet executeSelectQuery(Query query) {	
+		qExec = conn.query(query) ;
 
-		InputStream inputStream = null;
+		logger.info("Executing select SPARQL query:\n {}", query);
 
-		for(String file : files.subList(1, files.size())) {
-			try {
-				inputStream = new FileInputStream(file);
-				localReader.read(model, inputStream, file);
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			}
-		}
+		return qExec.execSelect();
+	}
 
-		inferenceModel = ModelFactory.createRDFSModel(model);
 
+
+	/**
+	 * Execute a CONSTRUCT SPARQL query using a read only or update SPARQL endpoint
+	 * @param queryString the SELECT SPARQL query as a String
+	 * @return the results of the query
+	 */
+	public Model executeConstructQuery(String queryString) {
+		Query query = QueryFactory.create(queryString) ;
+
+		return executeConstruct(query);
+	}
+
+	/**
+	 * Execute a CONSTRUCT SPARQL query using a read only or update SPARQL endpoint
+	 * @param queryString the SELECT SPARQL query as a String
+	 * @return the results of the query
+	 */
+	public Model executeConstruct(Query query) {
+		qExec = conn.query(query) ;
+		logger.info("Executing contruct SPARQL query:\n {}", query);
+		return qExec.execConstruct();
+	}
+
+	/**
+	 * Execute an update SPARQL query using an update SPARQL endpoint
+	 * @param queryString the SPARQL query to update the graph
+	 * @return the results of the query
+	 */
+	public void executeUpdateQuery(String queryString) {
+		executeUpdateQuery(queryString, endpoint);
+	}
+
+	/**
+	 * Execute an update SPARQL query using an update SPARQL endpoint
+	 * @param queryString the SPARQL query to update the graph
+	 * @param the SPARQL endpoint
+	 * @return the results of the query
+	 */
+	public void executeUpdateQuery(String queryString, String endpoint) {
+
+		logger.info("Executing update SPARQL query:\n {}", queryString);
+
+		UpdateRequest request = UpdateFactory.create(queryString) ;
+		UpdateProcessor processor = UpdateExecutionFactory.createRemote(request, endpoint);
+
+		processor.execute();
 	}
 
 	/**
@@ -118,85 +133,20 @@ public class JenaWrapper {
 		return qexec.execSelect();
 	}
 
-
-
-	/**
-	 * Execute a local update SPARQL query (DELETE, INSERT)
-	 * @param querythe SPARQL query to update the dataset	 
-	 * @throws FileNotFoundException 
-	 */
-	public void executeLocalUpdateQuery(String query) throws FileNotFoundException {
-
-		//Update the model that is used by this application
-		UpdateAction.parseExecute(query, model);
-		UpdateAction.parseExecute(query, updateModel);
-		UpdateAction.parseExecute(query, inferenceModel);
-
-		//Update the file by writing the new model value
-		OutputStream out = new FileOutputStream(new File(updateFile));
-		updateModel.write(out, DEFAULT_FORMAT);
-
-	}
-
-
-	/**
-	 * Execute a SELECT SPARQL query using a read SPARQL endpoint
-	 * @param queryString the SELECT SPARQL query
-	 * @return the results of the query
-	 */
-	public ResultSet executeRemoteSelectQuery(String queryString) {	
-		Query query = QueryFactory.create(queryString) ;
-		qExec = conn.query(query) ;
-		return qExec.execSelect();
-	}
-
-	/**
-	 * Should be called after iterating over the result of a query execution
-	 */
-	public void closeExecution() {
-		qExec.close();
-	}
-
-	/**
-	 * Execute a SELECT SPARQL query using a read SPARQL endpoint
-	 * @param queryString the SELECT SPARQL query
-	 * @return the results of the query
-	 */
-	public Model executeRemoteConstructQuery(String queryString) {
-		Query query = QueryFactory.create(queryString) ;
-
-		qExec = conn.query(query) ;
-		return qExec.execConstruct();
-	}
-
-	/**
-	 * Execute an update SPARQL query using a SPARQL update endpoint
-	 * @param queryString the SPARQL query to update the dataset
-	 * @param endpoint the URL of update SPARQL endpoint
-	 * @return the results of the query
-	 */
-	public void executeRemoteUpdateQuery(String queryString, String endpoint) {
-		logger.info("--- Update query ---");
-		logger.info(queryString);
-
-		UpdateRequest request = UpdateFactory.create(queryString) ;
-		UpdateProcessor processor = UpdateExecutionFactory.createRemote(request, endpoint);
-
-		processor.execute();
-	}
-
 	/**
 	 * Get the count of triples in the current database
 	 * @return
 	 */
 	public int getCount() {
 
+		logger.info("Counting triple for endpoint <{}>", endpoint);
+
 		String query = "SELECT (COUNT(*) as ?c)\r\n" + 
 				"WHERE{ \r\n" + 
 				"     ?s ?p ?o.\r\n" + 
 				"  }";
 
-		ResultSet results = executeLocalSelectQuery(query, model);
+		ResultSet results = executeSelectQuery(query);
 
 		return results.next().getLiteral("?c").getInt();
 	}
